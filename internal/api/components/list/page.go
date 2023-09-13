9dc723070ae4de21ede15fede0ce826a704cf8d2
IvanStukalov <ivanstukalov2003@gmail.com>
2023-09-13 11:00:40 +0300

    added styles

--- internal/api/components/list/page.go
+++ internal/api/components/list/page.go
@@ -21,11 +21,11 @@ func Render(url string, c *gin.Context) {
 				Image:       "sun.png",
 			},
 			{
-				Name:        "Солнце",
-				Description: "Наша родная звезда, которая светит нам и греет нас",
-				Distance:    0,
-				Magnitude:   -26.7,
-				Image:       "sun.webp",
+				Name:        "Проксима Центавра",
+				Description: "Звезда, красный карлик, относящаяся к звёздной системе Альфа Центавра, ближайшая к Солнцу звезда",
+				Distance:    4.2,
+				Magnitude:   11.1,
+				Image:       "Proxima_Centauri.jpg",
 			},
 		},
 	}
