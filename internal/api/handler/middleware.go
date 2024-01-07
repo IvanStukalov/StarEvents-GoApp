e365b1cc6d2f43e87c06dcfab405a25cfee21c3d
IvanStukalov <ivanstukalov2003@gmail.com>
2024-01-07 13:12:02 +0300

    fixes

--- internal/api/handler/middleware.go
+++ internal/api/handler/middleware.go
@@ -19,54 +19,73 @@ const (
 func (h *Handler) WithAuthCheck(assignedRoles []models.Role, allowedUnauth bool) func(ctx *gin.Context) {
 	return func(gCtx *gin.Context) {
 
-		if allowedUnauth {
-			gCtx.Set(userCtx, 0)
-			gCtx.Set(adminCtx, false)
-			gCtx.Next()
-		} else {
-			jwtStr, err := gCtx.Cookie("AccessToken")
-			if err != nil {
-				gCtx.AbortWithStatus(http.StatusForbidden) // отдаем что нет доступа
-				return
+		jwtStr, err := gCtx.Cookie("AccessToken")
+		if err != nil {
+			if allowedUnauth {
+				gCtx.Set(userCtx, 0)
+				gCtx.Set(adminCtx, false)
+				gCtx.Next()
 			}
-
-			if !strings.HasPrefix(jwtStr, jwtPrefix) { // если нет префикса то нас дурят!
-				gCtx.AbortWithStatus(http.StatusForbidden) // отдаем что нет доступа
-				return
-			}
-
-			jwtStr = jwtStr[len(jwtPrefix):]
-
-			err = h.redis.CheckJWTInBlacklist(gCtx.Request.Context(), jwtStr)
-			if err == nil { // значит что токен в блеклисте
-				gCtx.AbortWithStatus(http.StatusForbidden)
-				return
-			}
-			if !errors.Is(err, redis.Nil) {
-				gCtx.AbortWithError(http.StatusInternalServerError, err)
-
-				return
-			}
-
-			h.tokenManager.Parse(jwtStr)
-
-			userId, isAdmin, err := h.tokenManager.Parse(jwtStr)
-			if err != nil {
-				gCtx.AbortWithStatus(http.StatusForbidden)
-				return
-			}
-
-			if len(assignedRoles) == 1 {
-				if !isAdmin && assignedRoles[0] == 1 || isAdmin && assignedRoles[0] == 0 {
-					gCtx.AbortWithStatus(http.StatusForbidden)
-					log.Printf("user %v is not admin", userId)
-					return
-				}
-			}
-
-			gCtx.Set(userCtx, userId)
-			gCtx.Set(adminCtx, isAdmin)
-			gCtx.Next()
+			gCtx.AbortWithStatus(http.StatusForbidden) // отдаем что нет доступа
+			return
 		}
+
+		if !strings.HasPrefix(jwtStr, jwtPrefix) { // если нет префикса то нас дурят!
+			if allowedUnauth {
+				gCtx.Set(userCtx, 0)
+				gCtx.Set(adminCtx, false)
+				gCtx.Next()
+			}
+			gCtx.AbortWithStatus(http.StatusForbidden) // отдаем что нет доступа
+			return
+		}
+
+		jwtStr = jwtStr[len(jwtPrefix):]
+
+		err = h.redis.CheckJWTInBlacklist(gCtx.Request.Context(), jwtStr)
+		if err == nil { // значит что токен в блеклисте
+			if allowedUnauth {
+				gCtx.Set(userCtx, 0)
+				gCtx.Set(adminCtx, false)
+				gCtx.Next()
+			}
+			gCtx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+		if !errors.Is(err, redis.Nil) {
+			gCtx.AbortWithError(http.StatusInternalServerError, err)
+
+			return
+		}
+
+		h.tokenManager.Parse(jwtStr)
+
+		userId, isAdmin, err := h.tokenManager.Parse(jwtStr)
+		if err != nil {
+			if allowedUnauth {
+				gCtx.Set(userCtx, 0)
+				gCtx.Set(adminCtx, false)
+				gCtx.Next()
+			}
+			gCtx.AbortWithStatus(http.StatusForbidden)
+			return
+		}
+
+		if len(assignedRoles) == 1 {
+			if !isAdmin && assignedRoles[0] == 1 || isAdmin && assignedRoles[0] == 0 {
+				if allowedUnauth {
+					gCtx.Set(userCtx, 0)
+					gCtx.Set(adminCtx, false)
+					gCtx.Next()
+				}
+				gCtx.AbortWithStatus(http.StatusForbidden)
+				log.Printf("user %v is not admin", userId)
+				return
+			}
+		}
+
+		gCtx.Set(userCtx, userId)
+		gCtx.Set(adminCtx, isAdmin)
+		gCtx.Next()
 	}
 }
